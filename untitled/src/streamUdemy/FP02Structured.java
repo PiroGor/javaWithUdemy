2438a76f8d34592077d5e6b1b92af7943948576e
Anastasiia Nudha <nudhaa@outlook.com>
2024-01-22 13:02:00 +0100

    update FP02Structured.java

--- untitled/src/streamUdemy/FP02Structured.java
+++ untitled/src/streamUdemy/FP02Structured.java
@@ -9,18 +9,33 @@ public class FP02Structured {
 
         List<Integer> numbers = List.of(12,3,44,5,77,32,6,12,4);
 
+//        0 12
+//        12 3
+//        15 44
+//        59 5
+//        64 77
+//        141 32
+//        173 6
+//        179 12
+//        191 4
+//        195
+
         int sum= addListFunctional(numbers);
 
         System.out.println(sum);
+
     }
 
-    private static int sum(int a,int b){
-        return a+b;
+    private static int sum(int aggregate,int nextNumber){
+        System.out.println(aggregate+" "+nextNumber);
+        return aggregate+nextNumber;
     }
 
     private static int addListFunctional(List<Integer> numbers){
         return numbers.stream()
-                .reduce(0,FP02Structured::sum);
+                .reduce(0,Integer::sum);
+//                .reduce(0,(x,y) -> x+y);
+//                .reduce(0,FP02Structured::sum);
 //        int sum=0;
 //        for(int number:numbers){
 //            sum+=number;
