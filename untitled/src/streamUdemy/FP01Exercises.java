496ea6cf2854496845ed8fd48a33f08e3836d74b
Anastasiia Nudha <nudhaa@outlook.com>
2024-01-22 12:07:35 +0100

    update FP01Exercises.java

--- untitled/src/streamUdemy/FP01Exercises.java
+++ untitled/src/streamUdemy/FP01Exercises.java
@@ -5,24 +5,28 @@ import java.util.List;
 public class FP01Exercises {
     public static void main(String[] args){
         List<Integer> numbers=List.of(12,4,23,6,99,56,4);
-        printAllNumbersInLIstFunctional(numbers);
-        printEvenNumbersInLIstFunctional(numbers);
+        printOddNumbersInLIstFunctional(numbers);
+
+        List<String> courses=List.of("Spring", "Spring Boot", "API", "Microservices",
+                                     "AWS", "PCF", "Azure", "Docker", "Kubernetes");
+
+
+        courses.stream().forEach(System.out::println);
+
+        courses.stream()
+                .filter(course->course.contains("Spring"))
+                .forEach(System.out::println);
+
+        courses.stream()
+                .filter(course->course.length()==4)
+                .forEach(System.out::println);
     }
 
-//    private static boolean isEven(int number){
-//        return number%2==0;
-//    }
 
 
-    private static void printAllNumbersInLIstFunctional(List<Integer> numbers){
+    private static void printOddNumbersInLIstFunctional(List<Integer> numbers){
         numbers.stream()
-                .forEach(System.out::println); //Method Reference
-    }
-
-    private static void printEvenNumbersInLIstFunctional(List<Integer> numbers){
-        numbers.stream()
-                //.filter(FP01Functional::isEven)//Filter - Only even numbers
-                .filter(number -> number%2==0) //Lambda Expession
+                .filter(number -> number%2 != 0) //Lambda Expession
                 .forEach(System.out::println); //Method Reference
     }
 }
