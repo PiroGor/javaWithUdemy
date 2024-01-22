88b610c3507eb950aabdfe19759819bafaaa2e4b
Anastasiia Nudha <nudhaa@outlook.com>
2024-01-22 13:27:23 +0100

    update FP01Exercises.java

--- untitled/src/streamUdemy/FP01Exercises.java
+++ untitled/src/streamUdemy/FP01Exercises.java
@@ -13,6 +13,9 @@ public class FP01Exercises {
         exercisePart2(courses);
         exercisePart3_1(numbers);
         exercisePart3_2(courses);
+        System.out.println(exercisePart4_1(numbers));
+        System.out.println(exercisePart4_2(numbers));
+        System.out.println(exercisePart4_3(numbers));
     }
 
 
@@ -36,15 +39,32 @@ public class FP01Exercises {
     }
 
     private static void exercisePart3_1(List<Integer> numbers){
-        numbers.stream()
+        numbers.stream()                                    //exercise 5
                 .filter(number->number%2!=0)
                 .map(number->number*number*number)
                 .forEach(System.out::println);
     }
 
     private static void exercisePart3_2(List<String> courses){
-        courses.stream()
+        courses.stream()                                   //exercise 6
                 .map(course->course+" "+course.length())
                 .forEach(System.out::println);
+    }
+
+    private static int exercisePart4_1(List<Integer> numbers){
+        return numbers.stream()                            //exercise 7
+                .map(x->x*x)
+                .reduce(0,Integer::sum);
+    }
+
+    private static int exercisePart4_2(List<Integer> numbers){
+        return numbers.stream()                                 //exercise 8
+                .map(number->number*number*number)
+                .reduce(0,Integer::sum);
+    }
+    private static int exercisePart4_3(List<Integer> numbers){
+        return numbers.stream()                                   //exercise 9
+                .filter(x->x%2!=0)
+                .reduce(0,Integer::sum);
     }
 }
