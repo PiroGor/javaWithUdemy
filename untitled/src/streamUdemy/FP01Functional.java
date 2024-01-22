50764c199211cf1ea995d49b8207673f765b165e
Anastasiia Nudha <nudhaa@outlook.com>
2024-01-22 12:19:34 +0100

    update FP01Functional.java

--- untitled/src/streamUdemy/FP01Functional.java
+++ untitled/src/streamUdemy/FP01Functional.java
@@ -5,8 +5,9 @@ import java.util.List;
 public class FP01Functional {
     public static void main(String[] args){
         List<Integer> numbers=List.of(12,4,23,6,99,56,4);
-        printAllNumbersInLIstFunctional(numbers);
-        printEvenNumbersInLIstFunctional(numbers);
+        //printAllNumbersInLIstFunctional(numbers);
+       // printEvenNumbersInLIstFunctional(numbers);
+        printSquaresOfEvenNumbersInLIstFunctional(numbers);
     }
 
 //    private static boolean isEven(int number){
@@ -23,6 +24,13 @@ public class FP01Functional {
         numbers.stream()
                 //.filter(FP01Functional::isEven)//Filter - Only even numbers
                 .filter(number -> number%2==0) //Lambda Expession
+                .forEach(System.out::println); //Method Reference
+    }
+
+    private static void printSquaresOfEvenNumbersInLIstFunctional(List<Integer> numbers){
+        numbers.stream()
+                .filter(number -> number%2==0) //Lambda Expession
+                .map(number -> number*number)
                 .forEach(System.out::println); //Method Reference
     }
 }
