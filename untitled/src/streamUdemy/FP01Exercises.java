16cfeba55ac0c45179371566b5424d24f9e9cb5d
Anastasiia Nudha <nudhaa@outlook.com>
2024-01-22 11:59:25 +0100

    create FP01Exercises.java

---
+++ untitled/src/streamUdemy/FP01Exercises.java
@@ -0,0 +1,28 @@
+package streamUdemy;
+
+import java.util.List;
+
+public class FP01Exercises {
+    public static void main(String[] args){
+        List<Integer> numbers=List.of(12,4,23,6,99,56,4);
+        printAllNumbersInLIstFunctional(numbers);
+        printEvenNumbersInLIstFunctional(numbers);
+    }
+
+//    private static boolean isEven(int number){
+//        return number%2==0;
+//    }
+
+
+    private static void printAllNumbersInLIstFunctional(List<Integer> numbers){
+        numbers.stream()
+                .forEach(System.out::println); //Method Reference
+    }
+
+    private static void printEvenNumbersInLIstFunctional(List<Integer> numbers){
+        numbers.stream()
+                //.filter(FP01Functional::isEven)//Filter - Only even numbers
+                .filter(number -> number%2==0) //Lambda Expession
+                .forEach(System.out::println); //Method Reference
+    }
+}
