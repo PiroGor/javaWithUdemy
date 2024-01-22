0461f76582240065079b76403e2a8b5acc9653bf
Anastasiia Nudha <nudhaa@outlook.com>
2024-01-22 11:51:17 +0100

    isEven create

--- untitled/src/streamUdemy/FP01Functional.java
+++ untitled/src/streamUdemy/FP01Functional.java
@@ -4,15 +4,25 @@ import java.util.List;
 
 public class FP01Functional {
     public static void main(String[] args){
-        printAllNumbersInLIstFunctional(List.of(12,4,23,6,99,56,4));
+        List<Integer> numbers=List.of(12,4,23,6,99,56,4);
+        printAllNumbersInLIstFunctional(numbers);
+        printEvenNumbersInLIstFunctional(numbers);
     }
 
-    private static void print(int number){
-        System.out.println(number);
+    private static boolean isEven(int number){
+        return number%2==0;
     }
+
 
     private static void printAllNumbersInLIstFunctional(List<Integer> numbers){
         numbers.stream()
-                .forEach(FP01Functional::print); //Method Reference
+                .forEach(System.out::println); //Method Reference
+    }
+
+    private static void printEvenNumbersInLIstFunctional(List<Integer> numbers){
+        numbers.stream()
+
+                .filter(FP01Functional::isEven)//Filter - Only even numbers
+                .forEach(System.out::println); //Method Reference
     }
 }
--- untitled/src/streamUdemy/FP01Structured.java
+++ untitled/src/streamUdemy/FP01Structured.java
@@ -4,13 +4,26 @@ import java.util.List;
 public class FP01Structured {
 
     public static void main(String[] args){
+
+
         printAllNumbersInListStructured(List.of(12,4,23,6,99,56,4));
+        printEvenNumbersInListStructured(List.of(12,5,66,7,3,44,5,6));
     }
 
     private static void printAllNumbersInListStructured(List<Integer> numbers){
     for(int number:numbers){
         System.out.println(number);
      }
+    System.out.println();
+    }
+
+    private static void printEvenNumbersInListStructured(List<Integer> numbers){
+        for(int number:numbers){
+            if(number%2==0){
+                System.out.println(number);
+            }
+
+        }
     }
 
     
