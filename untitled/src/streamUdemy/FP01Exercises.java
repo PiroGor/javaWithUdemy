db6516870fc1efa118c3e904ed8050ad72cd99f5
Anastasiia Nudha <nudhaa@outlook.com>
2024-01-22 12:48:21 +0100

    update FP01Exercises.java

--- untitled/src/streamUdemy/FP01Exercises.java
+++ untitled/src/streamUdemy/FP01Exercises.java
@@ -10,23 +10,41 @@ public class FP01Exercises {
         List<String> courses=List.of("Spring", "Spring Boot", "API", "Microservices",
                                      "AWS", "PCF", "Azure", "Docker", "Kubernetes");
 
-
-        courses.stream().forEach(System.out::println);
-
-        courses.stream()
-                .filter(course->course.contains("Spring"))
-                .forEach(System.out::println);
-
-        courses.stream()
-                .filter(course->course.length()==4)
-                .forEach(System.out::println);
+        exercisePart2(courses);
+        exercisePart3_1(numbers);
+        exercisePart3_2(courses);
     }
 
 
 
     private static void printOddNumbersInLIstFunctional(List<Integer> numbers){
         numbers.stream()
-                .filter(number -> number%2 != 0) //Lambda Expession
+                .filter(number -> number%2 != 0) //Lambda Expression
                 .forEach(System.out::println); //Method Reference
+    }
+
+    private static void exercisePart2(List<String> courses){
+        courses.stream().forEach(System.out::println);      //Exercise 2
+
+        courses.stream()                                    //Exercise 3
+                .filter(course->course.contains("Spring"))
+                .forEach(System.out::println);
+
+        courses.stream()                                    //Exercise 4
+                .filter(course->course.length()==4)
+                .forEach(System.out::println);
+    }
+
+    private static void exercisePart3_1(List<Integer> numbers){
+        numbers.stream()
+                .filter(number->number%2!=0)
+                .map(number->number*number*number)
+                .forEach(System.out::println);
+    }
+
+    private static void exercisePart3_2(List<String> courses){
+        courses.stream()
+                .map(course->course+" "+course.length())
+                .forEach(System.out::println);
     }
 }
