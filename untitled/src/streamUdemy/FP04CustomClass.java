52d7f1cf6b52862cbba2e9d3f83e4ae00aff922a
Anastasiia Nudha <nudhaa@outlook.com>
2024-01-23 16:50:33 +0100

    update FP04CustomClass.java

--- untitled/src/streamUdemy/FP04CustomClass.java
+++ untitled/src/streamUdemy/FP04CustomClass.java
@@ -1,6 +1,11 @@
 package streamUdemy;
 
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
 class Course{
     private String name;
     private String category;
@@ -56,6 +61,115 @@ class Course{
 }
 public class FP04CustomClass {
     public static void main(String[] args){
+        List<Course> courses = List.of(
+                new Course("Spring", "Framework",98,20000),
+                new Course("Spring Boot", "Framework",95,18000),
+                new Course("API", "Microservices",97,22000),
+                new Course("Microservices", "Microservices",96,25000),
+                new Course("FullStuck", "FullStuck",91,14000),
+                new Course("AWS", "Cloud",92,21000),
+                new Course("Azure", "Cloud",99,21000),
+                new Course("Docker", "Cloud",92,2000),
+                new Course("Kubernetes", "Cloud",91,2000)
+        );
+
+        //allMatch, noneMatch, anyMatch
+        Predicate<Course> reviewScoreGreaterThan95Predicate = course -> course.getReviewScore() > 95;
+
+        Predicate<Course> reviewScoreGreaterThan90Predicate = course -> course.getReviewScore()>90;
+
+        Predicate<Course> reviewScoreLessThan90Predicate = course -> course.getReviewScore()<90;
+
+        System.out.println(
+                courses.stream().allMatch(reviewScoreGreaterThan95Predicate));
+
+        System.out.println(
+                courses.stream().noneMatch(reviewScoreGreaterThan90Predicate));
+
+        System.out.println(
+                courses.stream().anyMatch(reviewScoreLessThan90Predicate));
+
+        System.out.println(
+                courses.stream().anyMatch(reviewScoreGreaterThan95Predicate));
+
+        Comparator comparingByNoOFStudentsIncreasing = Comparator.comparing(Course::getNoOfStudents);
+
+        Comparator comparingByNoOFStudentsDecreasing = Comparator.comparing(Course::getNoOfStudents).reversed();
+
+        System.out.println(
+                courses.stream()
+                        .sorted(comparingByNoOFStudentsIncreasing)
+                        .collect(Collectors.toList()));
+
+        System.out.println(
+                courses.stream()
+                        .sorted(comparingByNoOFStudentsDecreasing)
+                        .collect(Collectors.toList()));
+
+        Comparator<Course> comparinByNoOfStudentsAndNoOfReview =
+                Comparator.comparing(Course::getNoOfStudents)
+                        .thenComparing(Course::getReviewScore)
+                        .reversed();
+
+        System.out.println(
+                courses.stream()
+                        .sorted(comparinByNoOfStudentsAndNoOfReview)
+                        .collect(Collectors.toList())
+        );
+
+        System.out.println(
+                courses.stream()
+                        .sorted(comparinByNoOfStudentsAndNoOfReview)
+                        .limit(5)
+                        .collect(Collectors.toList())
+        );
+
+        System.out.println(
+                courses.stream()
+                        .sorted(comparinByNoOfStudentsAndNoOfReview)
+                        .skip(3)
+                        .collect(Collectors.toList())
+        );
+
+        System.out.println(
+                courses.stream()
+                        .sorted(comparinByNoOfStudentsAndNoOfReview)
+                        .skip(3)
+                        .limit(5)
+                        .collect(Collectors.toList())
+        );
+
+        System.out.println(courses.stream()
+                .takeWhile(course -> course.getReviewScore()>=95)
+                .collect(Collectors.toList()));
+
+        System.out.println(
+                courses.stream()
+                        .filter(reviewScoreGreaterThan95Predicate)
+                        .mapToInt(Course::getNoOfStudents)
+                        .sum()
+        );
+
+        System.out.println(
+                courses.stream()
+                        .filter(reviewScoreGreaterThan95Predicate)
+                        .mapToInt(Course::getNoOfStudents)
+                        .average()
+        );
+
+        System.out.println(
+                courses.stream()
+                        .filter(reviewScoreGreaterThan95Predicate)
+                        .mapToInt(Course::getNoOfStudents)
+                        .count()
+        );
+
+        System.out.println(
+                courses.stream()
+                        .filter(reviewScoreGreaterThan95Predicate)
+                        .mapToInt(Course::getNoOfStudents)
+                        .max()
+        );
 
     }
 }
