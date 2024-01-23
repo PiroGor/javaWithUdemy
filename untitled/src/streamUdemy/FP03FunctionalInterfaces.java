0b5b290702fad4e88aa20e3f724c6e979344b3a3
Anastasiia Nudha <nudhaa@outlook.com>
2024-01-23 15:29:32 +0100

    update FP03FunctionalInterfaces.java

--- untitled/src/streamUdemy/FP03FunctionalInterfaces.java
+++ untitled/src/streamUdemy/FP03FunctionalInterfaces.java
@@ -1,56 +1,49 @@
 package streamUdemy;
 
 import java.util.List;
-import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.function.Predicate;
+import java.util.Random;
+import java.util.function.*;
 
 public class FP03FunctionalInterfaces {
 
-    /*
-    * boolean isEven(int x){
-    *   return x%2==0;
-    * }
-    *
-    * int squared(int x){
-    *   return x*x;
-    * }
-    *
-    **/
     @SuppressWarnings("unused")
     public static void main(String[] args){
         List<Integer> numbers=List.of(12,7,33,4,8,3,1,15);
 
         Predicate<Integer> isEvenPredicate = x -> x % 2 == 0;
 
-        Predicate<Integer> isEvenPredicate2 = new Predicate<Integer>() {
-            @Override
-            public boolean test(Integer x) {
-                return x % 2 == 0;
-            }
-        };
-
         Function<Integer, Integer> squareFunction = x -> x * x;
 
-        Function<Integer,Integer> squareFunction2 = new Function<Integer, Integer>() {
-            @Override
-            public Integer apply(Integer x) {
-                return x * x;
-            }
-        };
+        Function<Integer,String> stringOutputFunction = x -> x + " ";
 
         Consumer<Integer> sysoutConsumer = System.out::println;
 
-        Consumer<Integer> sysoutConsumer2 = new Consumer<Integer>() {
-            @Override
-            public void accept(Integer x) {
-                System.out.println(x);
-            }
+        BinaryOperator<Integer> sumBinaryOperator = (x,y) -> x + y;
+
+        //no input > return something
+        Supplier<Integer> randomIntegerSupplier=() -> {
+            Random random=new Random();
+            return random.nextInt(1000);
         };
 
-        numbers.stream()
-                .filter(isEvenPredicate)
-                .map(squareFunction)
-                .forEach(sysoutConsumer);
+        System.out.println(randomIntegerSupplier.get());
+
+        UnaryOperator<Integer> unaryOperator=x ->3 * x;
+
+        System.out.println(unaryOperator.apply(10));
+
+        BiPredicate<Integer, String> biPredicate = (number, str) -> {
+            return  number<10 && str.length()>5;
+        };
+
+        System.out.println(biPredicate.test(15,"eertdfs32r"));
+
+        BiFunction<Integer,String, String> biFunction=(number, str) -> {
+            return number + " " + str;
+        };
+
+        System.out.println(biFunction.apply(15,"sdfsfwefds"));
+
+
     }
 }
